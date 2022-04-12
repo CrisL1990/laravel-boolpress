64c5b3f8f4bab3a17254445514934b115ad88337
CrisL1990 <lucchetta.cris@gmail.com>
2022-04-12 15:01:36 +0200

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <Main/>
+</template>
+
+<script>
+  import Main from '../components/Main';
+
+  export default {
+    name: 'App',
+
+    components: {
+      Main,
+    },
+
+
+
+  }
+
+</script>
+
+<style>
+
+</style>
